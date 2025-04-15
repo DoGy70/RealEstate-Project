2bea85ce7581b153c72a273f126e2e4d5b405c2d
DoGy70 <thedarkofficer99@gmail.com>
2025-04-15 15:02:51 +0300

    removed seed button

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -5,7 +5,6 @@ import FeaturedProperties from "@/components/FeaturedProperties";
 import Filters from "@/components/Filters";
 import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import AdditionalFilters from "@/components/AdditionalFilters";
-import seed from "@/lib/seed";
 
 export default function HomeScreen() {
   const actionSheetRef = useRef<ActionSheetRef>(null);
@@ -15,12 +14,7 @@ export default function HomeScreen() {
       ListHeaderComponent={
         <View>
           <FeaturedProperties actionSheetRef={actionSheetRef} />
-          <TouchableOpacity
-            className="bg-green-100 ml-3"
-            onPress={() => seed()}
-          >
-            <Text>Seed</Text>
-          </TouchableOpacity>
+
           <Filters />
         </View>
       }
